2407f5ea0e35f4c8844fc36f4d0d7c000ab8114e
Fogman12 <65825164+Fogman12@users.noreply.github.com>
2020-07-02 01:16:06 -0400

    Updated app.js (MVC Framework)
    Please make sure you run the "npm install" command to ensure you have the latest dependencies installed

--- FrontEndStuff/app.js
+++ FrontEndStuff/app.js
@@ -1,15 +1,42 @@
 const express = require('express');
 const morgan = require('morgan');
+const mysql = require('mysql');
 
 const regRoutes = require('./routes/registrationRoutes');
 const loginRoutes = require('./routes/loginRoutes');
 const busRoutes = require('./routes/busRoutes');
 const createSurveyRoutes = require('./routes/createSurveyRoutes');
+const takeSurveyRoutes = require('./routes/takeSurveyRoutes');
+const bodyParser = require('body-parser');
+
+
+
+
+//create connetion
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Pass12345',
+    database : 'BSA_Database'
+});
+
+// connect server to database
+db.connect ((err) => {
+    if(err)
+    {
+        throw err;
+    } else
+    {
+        console.log('MySQL connected ...');
+    }
+});
 
 
 //express app
 
 const app = express();
+
+//connect to mongoDB
 
 //register view engine
 app.set('view engine', 'ejs');
@@ -23,10 +50,13 @@ app.listen(3000,()=>{
 //static files - CSS and Images
 app.use(express.static('css'));
 app.use(express.static('images'));
-//app.use(express.urlencoded({extended:true})); // Used for sending chunk of data to database, used with the form data.
+app.use(express.urlencoded({extended:true})); // Used for sending chunk of data to database, used with the form data.
+app.use(bodyParser.json());
+
 
 //log incoming request 
 app.use(morgan('tiny'));
+
 
 //*********Redirects for the actual pages *************************/
 
@@ -50,11 +80,25 @@ app.use('/registration', regRoutes);
 app.use('/busOwn', busRoutes);
 
 //create survey routes
-app.use('/createSurvey', createSurveyRoutes);
+//app.use('/createSurvey', createSurveyRoutes);
 
-app.get('/TakeSurvey', (req, res) =>{
-    res.render('TakeSurvey');
-})
+app.get ('/getCustomers',(req, res) => {
+    let sql = "call BSA_Database.GetSurveyQuestions('4')";
+    
+    db.query (sql, (err, result) => {
+        if (err)
+        {
+            throw err;
+        } else
+        {
+            console.log("Data found .....");
+            res.send(result);
+        }
+    });
+});
+
+//take survey routes
+app.use('/TakeSurvey', takeSurveyRoutes);
 
 //Redirect if no matches with above -> will show 404 error page
 app.use((req, res) => {
