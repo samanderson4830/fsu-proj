954b942ad19498014aa0e144ac1bdc052c29a825
Fogman12 <65825164+Fogman12@users.noreply.github.com>
2020-07-02 01:19:23 -0400

    Create db_connection.js
    This allows us the "model" in the MVC framework to connect to the database.

---
+++ FrontEndStuff/models/db_connection.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+
+// create connetion
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Pass12345',
+    database : 'BSA_Database'
+});
+
+// connect server to database
+db.connect ((err) => {
+    if(err)
+    {
+        throw err;
+    } else
+    {
+        console.log('MySQL connected ...');
+    }
+});
+
+module.exports = db;
