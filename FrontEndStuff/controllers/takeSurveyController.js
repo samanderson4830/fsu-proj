1ce10d805e86c9a92c7f4c11953bfa13f3bb3a04
Fogman12 <65825164+Fogman12@users.noreply.github.com>
2020-07-04 00:00:51 -0400

    Updated controllers

--- FrontEndStuff/controllers/takeSurveyController.js
+++ FrontEndStuff/controllers/takeSurveyController.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const db = require('../models/db_connection');
+const { render } = require('ejs');
 
 
 const takeSurvey_page = (req,res) => {
@@ -8,12 +9,37 @@ const takeSurvey_page = (req,res) => {
 
 const takeSurvey_code = (req,res) => {
     const code = req.query.code;
-    db.query('SELECT * FROM surveys_created WHERE survey_Id = ?', code, (err,result) => {
+    db.query('SELECT survey_name FROM surveys_created WHERE survey_Id = ?', code, (err,result) => {
         if(err)
         throw err;
-        else
-        res.send(result);
+        else{
+            //res.send(result);
+            //var test1 = JSON.parse(result);
+
+            obj = result[0].survey_name;
+            console.log(obj);
+            db.query('SELECT survey_ID FROM surveys_created WHERE survey_name = ?', obj, (err,thisResult) => {
+                if(err)
+                throw err;
+                else{
+                    //res.send(result);
+                    
+                    var test1 = {};
+                    test1 = thisResult[0].survey_ID;
+                    console.log(test1);
+                    res.redirect('../survey');
+                }
+            })
+            //res.redirect('../survey');
+            //**** Now need to send this survey name to the survey page and make it generic. */
+            // res.render('surveyPage', {
+            //     _name : result[0].survey_name
+            // });
+        }
     })
+
+    
+
 
     // let sql = "call BSA_Database.GetSurveyQuestions('?')", code;
 
