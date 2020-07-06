108d05afa6d86d8accd6ef806e511f9d34b3884d
samanderson4830 <50926328+samanderson4830@users.noreply.github.com>
2020-07-06 12:00:59 -0400

    created new functions; updated tables; created new procedures; added new temp data

--- node-mysql/BSA_Database.sql
+++ node-mysql/BSA_Database.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS `BSA_Database`.`surveys_created`(
     `survey_ID` INT NOT NULL AUTO_INCREMENT, 
     `customer_ID` INT NOT NULL, 
     `survey_name` VARCHAR(30) NOT NULL, 
-    `date_created` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP, 
+    `date_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP, 
     PRIMARY KEY(`survey_ID`), 
     INDEX `idx.customer` (customer_ID),
     CONSTRAINT `fk_customer_ID` 
@@ -50,14 +50,14 @@ CREATE TABLE IF NOT EXISTS `BSA_Database`.`answers`(
     PRIMARY KEY(`answer_ID`,`question_ID`)
 )ENGINE = InnoDB;
 
+
 CREATE TABLE `BSA_Database`.`survey_results`(
-    `result_ID` INT NOT NULL AUTO_INCREMENT, 
+    `taker_ID` INT NOT NULL, 
     `survey_ID` INT NOT NULL,
-    `answer_ID` INT NOT NULL, 
-    `taker_ID` INT NOT NULL , 
-    `is_done` BOOLEAN NOT NULL, 
+    `answer_ID` INT NOT NULL,  
+    `is_done` BOOLEAN, 
     `date_time`  TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP, 
-    PRIMARY KEY(`result_ID`), 
+	KEY (`taker_ID`, survey_ID),
     INDEX `idx.answer_ID` (answer_ID),
     CONSTRAINT `fk_answer_ID` 
     FOREIGN KEY(`answer_ID`) REFERENCES answers(`answer_ID`) ON UPDATE CASCADE ON DELETE RESTRICT
@@ -111,39 +111,75 @@ INSERT INTO `surveys_created`(`customer_ID`,`survey_name`) VALUES
 -------------------------------------------------------------------------------*/
  INSERT INTO `answers`(`question_ID`,`answer_string`, `answer_order`) VALUES
 -- Home Depot Performance Survey
+# Ans for Q1
 (1,    'Great', 1),
 (1,    'Good',  2),
 (1,    'ok',    3),
 (1,    'poor',  4),
-
+# Ans for Q2
 (2,    'YES ', 1),
 (2,    'NO',   2),
 
 -- Home Depot Employee Survey
+# Ans for Q1
 (3,    'Yes, home depot is great', 1),
 (3,    'No, I want a new job',     2),
 
 -- Windows Vista Survey
+# Ans for Q1
 (4,    'Yes, its great', 1),
 (4,    'No!',            2),
 
 -- BACONATOR Review
+# Ans for Q1
 (5,    'less than 1lb', 1),
 (5,    '1lb - 2lb'    , 2),
 (5,    'more than 3lb', 3),
 
+# Ans for Q2
 (6,    'Yes!',          1),
 (6,    'No!',           2),
 
+# Ans for Q3
 (7,    'More Bacon!',          1),
 (7,    'Nothing its great!',   2);
+
+/*-----------------------------------------------------------------------------
+-- Creating Default temporay Data for survey_results Table
+-------------------------------------------------------------------------------*/
+ INSERT INTO `survey_results`(`taker_ID`,`survey_ID`,`answer_ID`, `is_done`) VALUES
+-- Home Depot Performance Survey Answers / Has (2) questions
+(1,      1,     2,     NULL),  # new taker 1
+(1,      1,     1,     true),
+
+(2,      1,     2,     NULL),  # new taker 2
+(2,      1,     2,     true),
+
+-- Home Depot Employee Survey Answers / Has (1) question
+(3,      2,    7,     true),   # new taker 3
+(4,      2,    7,     true),   # new taker 4
+(5,      2,    8,     true),   # new taker 5
+
+-- Windows Vista Survey Answers / Has (1) question
+(6,      3,    9,     true),   # new taker 6
+(7,      4,    10,    true),   # new taker 7
+(8,      5,    9,     true),   # new taker 8
+
+-- BACONATOR Review Survey Answers / Has (3) question
+(9,      3,    11,    NULL),   # new taker 9
+(9,      4,    15,    NULL),   
+(9,      5,    16,    true),   
+
+(10,     3,    12,    NULL),   # new taker 10
+(10,     4,    14,    NULL),   
+(10,     5,    16,    true); 
 
 /*-----------------------------------------------------------------------------
 -- Creating all PROCEDURE
 -------------------------------------------------------------------------------*/
 
 /*-----------------------------------------------------------------------------
---  PROCEDUREs to get information
+--  PROCEDUREs for customer table
 -------------------------------------------------------------------------------*/
 DROP PROCEDURE IF EXISTS `GetCustomersAll`;
 
@@ -173,19 +209,19 @@ DROP PROCEDURE IF EXISTS `GetCustomersByName`;
 
 DELIMITER $$
 USE `BSA_Database`$$
-CREATE PROCEDURE `GetCustomersByName` (IN cName VARCHAR(100))
+CREATE PROCEDURE `GetCustomersByName` (IN cName VARCHAR(100) )
 BEGIN
    SELECT * FROM customers WHERE company_name = cName;
 END$$
 
 DELIMITER ;
-/*-----------------------------------------------------------------------------*/
 
+/*-----------------------------------------------------------------------------*/
 DROP PROCEDURE IF EXISTS `GetCustomersByEmail`;
 
 DELIMITER $$
 USE `BSA_Database`$$
-CREATE PROCEDURE `GetCustomersByEmail` (IN cEmail VARCHAR(100))
+CREATE PROCEDURE `GetCustomersByEmail` (IN cEmail VARCHAR(100) )
 BEGIN
    SELECT * FROM customers WHERE email = cEmail;
 END$$
@@ -221,7 +257,7 @@ DROP PROCEDURE IF EXISTS `AddSurvey`;
 
 DELIMITER $$
 USE `BSA_Database`$$
-CREATE PROCEDURE `AddSurvey` (IN cID INT, IN sName VARCHAR(100))
+CREATE PROCEDURE `AddSurvey` (IN cID INT, IN sName VARCHAR(100) )
 BEGIN
    INSERT INTO surveys_created (customer_ID, survey_name) VALUES (cID, sName);
 END$$
@@ -264,6 +300,7 @@ USE `BSA_Database`$$
 CREATE PROCEDURE `AddQuestions` (IN sID INT, IN qOrder INT, IN qString VARCHAR(250) )
 BEGIN
     INSERT INTO questions (survey_ID, question_order, question_string) VALUES (sID, qOrder, qString);
+    
 END$$
 
 DELIMITER ;
@@ -282,6 +319,18 @@ BEGIN
 END$$
 
 DELIMITER ;
+/*-----------------------------------------------------------------------------*/
+DROP PROCEDURE IF EXISTS `GetAnswerStringByID`;
+
+DELIMITER $$
+USE `BSA_Database`$$
+CREATE PROCEDURE `GetAnswerStringByID` (IN aID INT)
+BEGIN
+   SELECT answer_string FROM answers WHERE answer_ID = aID; 
+ 
+END$$
+
+DELIMITER ;
 
 /*-----------------------------------------------------------------------------*/
 DROP PROCEDURE IF EXISTS `AddAnswer`;
@@ -296,10 +345,40 @@ END$$
 
 DELIMITER ;
 
-
 /*-----------------------------------------------------------------------------
 --  PROCEDUREs for survey_results table
 -------------------------------------------------------------------------------*/
+DROP PROCEDURE IF EXISTS `GetResultsFromTaker`;
+
+DELIMITER $$
+USE `BSA_Database`$$
+CREATE PROCEDURE `GetResultsFromTaker` (IN tID INT)
+BEGIN
+   SELECT * FROM survey_results WHERE taker_ID = tID; 
+ 
+END$$
+
+DELIMITER ;
+
+/*-----------------------------------------------------------------------------*/
+DROP PROCEDURE IF EXISTS `AddResult`;
+
+DELIMITER $$
+USE `BSA_Database`$$
+CREATE PROCEDURE `AddResult` (IN aID INT, IN sID INT, IN qID INT)
+BEGIN
+   DECLARE done BOOLEAN DEFAULT NULL;
+
+   # check if survey is done 
+		IF IsSurveyFinished (qID, aID) = TRUE THEN
+			SET done = TRUE; 
+		END IF;
+   # if false done will remain null
+   INSERT INTO survey_results (answer_ID, survey_ID, is_done) VALUES (aID, sID, done);
+ 
+END$$
+
+DELIMITER ;
 
 /*-----------------------------------------------------------------------------
 --  All Functions 
@@ -307,20 +386,18 @@ DELIMITER ;
 DROP FUNCTION IF EXISTS `ValidLogin`;
 
 DELIMITER $$
-CREATE FUNCTION `ValidLogin` (cEmail VARCHAR(100))  RETURNS BOOL DETERMINISTIC
+CREATE FUNCTION `ValidLogin` (cEmail VARCHAR(100) ) RETURNS BOOL DETERMINISTIC
 BEGIN
-      # email exists flag
-      DECLARE is_valid BOOLEAN DEFAULT FALSE;
-
-      # email count will be 1 if true
-      DECLARE email_count INT DEFAULT 0;
+      # variables 
+      DECLARE is_valid BOOLEAN DEFAULT FALSE;  # email exists flag
+      DECLARE email_count INT DEFAULT 0;       # email count will be 1 if true
 
       # check for exsiting email in customer table 
             SELECT COUNT(1) INTO email_count
             FROM  `customers`
             WHERE email = cEmail;
 
-            # set if email exists
+	  # set true if email exists assumed all emials are unique
             IF email_count = 1 THEN
                   SET is_valid = TRUE;
             END IF;
@@ -330,3 +407,66 @@ END $$
 
 DELIMITER ;
 
+/*-----------------------------------------------------------------------------*/
+DROP FUNCTION IF EXISTS `FindQuestionID`;
+
+DELIMITER $$
+CREATE FUNCTION `FindQuestionID` (aID INT) RETURNS INT DETERMINISTIC
+BEGIN
+	  # variables 
+      DECLARE qID INT DEFAULT -1;
+      
+	  # match answer_ID to corresponding question
+            SELECT question_ID INTO qID
+            FROM  `answers`
+            WHERE answer_ID = aID;
+            
+	  # if not found retuns -1
+      RETURN qID;
+END $$
+
+DELIMITER ;
+
+/*-----------------------------------------------------------------------------*/
+DROP FUNCTION IF EXISTS `TotalQuestions`;
+
+DELIMITER $$
+CREATE FUNCTION `TotalQuestions` (sID INT) RETURNS INT DETERMINISTIC
+BEGIN
+      # variables 
+      DECLARE total INT DEFAULT -1;
+      
+	  # match answer_ID to corresponding question
+            SELECT COUNT(survey_ID) INTO total
+            FROM  `questions`
+            WHERE survey_ID = sID;
+            
+	  # total num of questions in a survey
+      RETURN total;
+END $$
+
+DELIMITER ;
+
+/*-----------------------------------------------------------------------------*/
+DROP FUNCTION IF EXISTS `IsSurveyFinished`;
+
+DELIMITER $$
+CREATE FUNCTION `IsSurveyFinished` (qID INT, sID INT)  RETURNS BOOL DETERMINISTIC
+BEGIN
+	  # variables 
+      DECLARE total INT DEFAULT totalQuestions (sID); # set total to total number of question in a survey
+      DECLARE num INT DEFAULT -1; 
+	  DECLARE done BOOLEAN DEFAULT FALSE;
+      
+	  # set values of num 
+			SELECT question_order INTO num FROM questions WHERE question_ID = qID; 
+            
+	  # if last question is being answered and question exits
+			IF num = total && num != -1 THEN
+				SET done = TRUE;
+			END IF;
+      
+      RETURN done;
+END $$
+
+DELIMITER ;
