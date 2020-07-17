f18579ac8964c04fed88caf0ef7b025175de5fe0
Fogman12 <65825164+Fogman12@users.noreply.github.com>
2020-07-16 22:11:58 -0400

    Updated takeSurveyRoutes-pull questions & answers

--- FrontEndStuff/routes/takeSurveyRoutes.js
+++ FrontEndStuff/routes/takeSurveyRoutes.js
@@ -4,5 +4,8 @@ const router = express.Router();
 
 router.get('/',takeSurveyController.takeSurvey_page);
 router.get('/survey', takeSurveyController.takeSurvey_code);
+router.get('/questions', takeSurveyController.get_questions);
+router.get('/answers', takeSurveyController.get_answers);
+router.get('/offset', takeSurveyController.get_offset);
 
 module.exports = router;
