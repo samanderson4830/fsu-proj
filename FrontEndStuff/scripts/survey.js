8a96ff3cc2e3329b52f67062bb50aedba4b45da9
Fogman12 <65825164+Fogman12@users.noreply.github.com>
2020-07-21 21:59:01 -0400

    POST request - complete
    Updated the client side script to now send POST request selected answers to the database

--- FrontEndStuff/scripts/survey.js
+++ FrontEndStuff/scripts/survey.js
@@ -1,3 +1,4 @@
+
 const startButton = document.getElementById('start-btn');
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
@@ -6,16 +7,17 @@ const nextButton = document.getElementById('next-btn')
 const thankButton = document.getElementById('thank-btn');
 var totalQuestions
 let currentQuestionIndex, questionArray, currentAnswerArray
+var tempAnsArray, submitAnswersArray = [];
 
 //*********************************************************************************** */
 const db_totalQuestions = new XMLHttpRequest();
 db_totalQuestions.open('GET', 'http://localhost:3000/TakeSurvey/offset');
 
 db_totalQuestions.onload = function(){
-    console.log('GetRequestTest');
-    console.log(db_totalQuestions.responseText);
+    //console.log('GetRequestTest');
+    //console.log(db_totalQuestions.responseText);
     totalQuestions = parseInt(db_totalQuestions.responseText);
-    console.log("Worked....");
+    //console.log("Worked....");
 };
 db_totalQuestions.send();
 
@@ -26,10 +28,10 @@ const dbRequest = new XMLHttpRequest();
 dbRequest.open('GET', 'http://localhost:3000/TakeSurvey/questions');
 
 dbRequest.onload = function(){
-    console.log('GetRequestTest');
-    console.log(dbRequest.responseText);
+   // console.log('GetRequestTest');
+   // console.log(dbRequest.responseText);
     questionArray = JSON.parse(dbRequest.responseText);
-    console.log(questionArray[0].question_string);
+    //console.log(questionArray[0].question_string);
 };
 dbRequest.send();
 
@@ -40,10 +42,10 @@ const dbAnswer = new XMLHttpRequest();
 dbAnswer.open('GET', 'http://localhost:3000/TakeSurvey/answers');
 
 dbAnswer.onload = function(){
-    console.log('GetRequestTest');
-    console.log(dbAnswer.responseText);
+    //console.log('GetRequestTest');
+    //console.log(dbAnswer.responseText);
     currentAnswerArray = JSON.parse(dbAnswer.responseText);
-    console.log(currentAnswerArray[0].answer_string);
+    //console.log(currentAnswerArray[0].answer_string);
 };
 dbAnswer.send();
 
@@ -64,6 +66,7 @@ function startSurvey() {
     //
     startButton.classList.add('hide')
     currentQuestionIndex = 0
+    submitAnswersArray, tempAnsArray = [];
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
 
@@ -77,10 +80,10 @@ function setNextQuestion(){
     dbAnswer.open('GET', 'http://localhost:3000/TakeSurvey/answers');
 
     dbAnswer.onload = function(){
-        console.log('GetRequestTest');
-        console.log(dbAnswer.responseText);
+        //console.log('GetRequestTest');
+       // console.log(dbAnswer.responseText);
         currentAnswerArray = JSON.parse(dbAnswer.responseText);
-        console.log(currentAnswerArray[0].answer_string);
+        //console.log(currentAnswerArray[0].answer_string);
     };
     dbAnswer.send();
 
@@ -112,6 +115,7 @@ function resetState(){
 
 function selectAnswer(e){
     const selectedButton = e.target
+    addToArray(selectedButton)
     //const sendData =
     // Array.from(answerButtonsElement.children).forEach(button=>{
         
@@ -122,6 +126,31 @@ function selectAnswer(e){
     else{
         // startButton.innerText = 'Thank You!'
         thankButton.classList.remove('hide')
+        //http request to send array of answers out to database
+        var i;
+        for(i = 0; i < tempAnsArray.length; i++)
+        {submitAnswersArray.push(tempAnsArray[i].innerText)}
+        console.log(submitAnswersArray);
+        var myJson = JSON.stringify(submitAnswersArray);
+//***************************************************************** */
+        const dbSendResult = new XMLHttpRequest();
+        dbSendResult.open('POST', 'http://localhost:3000/TakeSurvey/sendResult', true);
+        dbSendResult.setRequestHeader('Content-type', 'application/json');
+        dbSendResult.onload = function () {
+            var test = JSON.parse(dbSendResult.responseText);
+            if (dbSendResult.readyState == 4 && dbSendResult.status == "201") {
+                console.table(test);
+            } else {
+                console.error(test);
+            }
+        }
+        dbSendResult.send(myJson);
+
+//***************************************************************** */
         
     }
+}
+
+function addToArray(choice){
+    tempAnsArray.push(choice);
 }
