30d206c001942ad565033192ab8ec203ab585acb
Luis Corps <luiscorps@luiss-mbp.attlocal.net>
2020-07-26 23:03:30 -0400

    registration page now successfully adds new user into database

--- FrontEndStuff/controllers/registrationController.js
+++ FrontEndStuff/controllers/registrationController.js
@@ -1,23 +1,25 @@
-//const Customer = require('../models/customer');
+const mysql = require('mysql');
+const db = require('../models/db_connection');
+const { render } = require('ejs');
 
 const registration_page = (req,res) => {
     res.render('registration');
 }
 
-// const registration_new = (req, res) => {
-//     const customer = new Customer(req.body);
-
-//     customer.save()
-//     .then((result) => {
-//         res.redirect('/Login'); //redirects to login page
-//     })
-//     .catch((err) => {
-//         console.log(err);
-        
-//     })
-// }
+const registration_new = function (req, res) {
+    //call AddCompany procedure and pass in value that are coming in via form inputs
+    db.query("CALL AddCompany(?,?,?,?,?)", [req.body.email, req.body.code, req.body.companyName, req.body.firstName,
+        req.body.lastName], function(err, result) {
+        if (err) throw err;
+        else {
+            console.log(result[0]);
+        }
+    });
+    //after account is made we redirect them to the login page
+    res.redirect('/login');
+}
 
 module.exports = {
-   // registration_new,
+    registration_new,
     registration_page
 }
--- FrontEndStuff/routes/registrationRoutes.js
+++ FrontEndStuff/routes/registrationRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const registrationController = require('../controllers/registrationController');
 const router = express.Router();
 
+//ROUTES
+//main registration page
 router.get('/', registrationController.registration_page);
-
-//router.post('/', registrationController.registration_new);
+//post request page where new account info is processed
+router.post('/newUser', registrationController.registration_new);
 
 module.exports = router;
