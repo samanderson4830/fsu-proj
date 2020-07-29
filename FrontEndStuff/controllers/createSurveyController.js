06c569ff1c3d92da86c90e936229041462ace97b
Luis Corps <luiscorps@luiss-mbp.attlocal.net>
2020-07-29 10:32:24 -0400

    fixed createSurveyController so the surveyName and surveyID is updated in object array

--- FrontEndStuff/controllers/createSurveyController.js
+++ FrontEndStuff/controllers/createSurveyController.js
@@ -13,10 +13,21 @@ const create_new = function (req, res) {
     db.query("CALL AddSurvey(?, ?, ?, ?)", [customerID, newSurveyTitle, newSurveyDescription, 0], function (err, result) {
         if(err) throw err;
         else {
-            surveyNames.push(newSurveyTitle);
+            // surveyNames.push(newSurveyTitle); //with new object array this won't work
             surveyDescriptions.push(newSurveyDescription);
             addingToSurvey = newSurveyTitle;
-            res.redirect("/addQuestions");
+            db.query("CALL GetSurveyCustomerByID(?)", [customerID], function(err, result){
+                if(err) throw err;
+                else {
+                    let index = surveyNames.length;
+                    // console.log(result[0][index]); //grabs current survey info (need the survey_ID for object array)
+                    surveyNames.push({
+                        surveyName: newSurveyTitle,
+                        surveyCode: result[0][index].survey_ID
+                    });
+                    res.redirect("/addQuestions");
+                }
+            });
         }
     });
 }
